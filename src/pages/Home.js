9ea7c1763e7df2d9074ab898f77db1f8a8acc90b
wvxbs <gabriel.ferreira7854@gmail.com>
2022-11-09 03:41:16 -0300

    sample forms added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
 import Header from "../components/Header";
+import Form1 from "./home/Form1";
+import Form2 from "./home/Form2";
+import Form3 from "./home/Form3";
 
 function Home() {
     return (
       <div className="App">
         <Header />
         <div className="container">
-          <h1>Home</h1>
+          <h1>Cadastro</h1>
+          <Form1 />
+          <Form2 />
+          <Form3 />
         </div>
       </div>
     );
---
+++ src/pages/home/Form1.js
@@ -0,0 +1,43 @@
+import { useState } from "react"
+
+const Form1 = () => {
+
+    const [form, setForm] = useState({
+        example1: '',
+        example2: '',
+        example3: ''
+      })
+
+     return (
+        <div className="mb-3">
+            <label className="form-label">Nome</label>
+            <input type="text" className="form-control" placeholder="nome" 
+                onChange={e => {
+                    setForm({
+                        ...form,
+                        example1: e.target.value
+                    })
+                }}
+            />
+            <label className="form-label">Email</label>
+            <input type="text" className="form-control" placeholder="exemplo@email.com" 
+                onChange={e => {
+                    setForm({
+                        ...form,
+                        example2: e.target.value
+                    })
+                }}
+            />
+            <label className="form-label">Senha</label>
+            <input type="password" className="form-control" placeholder="..." 
+                onChange={e => {
+                    setForm({
+                        ...form,
+                        example3: e.target.value
+                    })
+                }}
+            />
+        </div>
+    )
+}
+export default Form1
