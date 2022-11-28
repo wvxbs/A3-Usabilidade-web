78beab874efdae4567c6ba767f86539ebc42b5d3
wvxbs <gabriel.ferreira7854@gmail.com>
2022-11-28 10:56:41 -0300

    card updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,9 +11,9 @@ const Card = props => {
     }
 
     const DeletePerson = () => {
-        const id = props.id
+        const id = props.data.id
 
-        fetch(process.env.REACT_APP_API_URL + "/person/delete?id=" + id, {
+        fetch(process.env.REACT_APP_API_URL + "/person?id=" + id, {
             method: 'DELETE',
             mode : 'cors'
         })
@@ -30,19 +30,19 @@ const Card = props => {
     }
 
     return (
-        <div className="card bg-dark" key={props.id}>
+        <div className="card bg-dark" key={props.data.id}>
         <div className="card-header">
-            <h5 className="card-title"><b>{props.name}</b> {props.age}</h5>
+            <h5 className="card-title text-wrap"><b>{props.data.name}</b> {props.data.age}</h5>
         </div>
         <div className="card-body">
-            <p className="card-text">{props.summary}</p>
+            <p className="card-text text-wrap">{props.data.summary}</p>
         </div>
         <div className="card-footer">
             <div className="btn-group" role="group">
                 <button className="btn btn-danger" onClick={handleclick}>
                     <i className="fa-solid fa-trash"></i>
                 </button>
-                <a href={props.link} className="btn btn-success">
+                <a href={props.data.link} className="btn btn-success">
                     <i className="fa-solid fa-link"></i>
                 </a>
             </div>
