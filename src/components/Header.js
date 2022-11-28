22b4f7b77d54e367ddde29d13abade2890f75819
wvxbs <gabriel.ferreira7854@gmail.com>
2022-11-28 11:16:20 -0300

    Header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = props => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
             <div className="container-fluid">
-                <Link to="/" className="navbar-brand">Cancelados</Link>
+                <Link to="/" className="navbar-brand">Cancelpedia</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
