a6f4f163eae783600e8f3212f1da384205f9f2a9
wvxbs <gabriel.ferreira7854@gmail.com>
2022-11-29 00:34:52 -0300

    Searchbox updated

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -9,12 +9,13 @@ const Searchbox = ()     => {
     const handleSubmit = event => {
         event.preventDefault();
 
-        return navigate("/search?query=" + Query)
+         navigate(`../search?query=${Query}`, {replace : true})
+         navigate(0)
     }
 
     return (
         <form className="form-inline my-2 my-lg-0 d-inline" onSubmit={handleSubmit} >
-            <input className="form-control custom-search mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar"                 
+            <input className="form-control custom-search mr-sm-2" type="search" placeholder="Buscar por nome..." aria-label="Buscar por nome..."                 
                 onChange={e => setQuery(e.target.value)}
             />
         </form>
