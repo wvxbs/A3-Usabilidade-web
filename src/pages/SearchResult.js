3f4832812b83a59a8bf03e4216f6e561a51a7b8e
wvxbs <gabriel.ferreira7854@gmail.com>
2022-11-28 23:30:32 -0300

    SearchResults page updated

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -1,40 +1,49 @@
-import { useState,useEffect } from 'react'
+import { useEffect, useState } from "react"
 import { useSearchParams } from 'react-router-dom'
-import Card from '../components/Card'
+import Card from "../components/Card"
 import Header from "../components/Header"
-import Loader from '../components/Loader'
-import MissingData from '../components/MissingData'
+import Loader from "../components/Loader"
+import MissingData from "../components/MissingData"
 
-const SearchResult = () => {
+const SearchResult = () => { 
+
     const [searchParams] = useSearchParams()
-    const [Query, setQuery] = useState("")
-    const [CardData, setCardData] = useState(undefined)
+    const [Query, setQuery] = useState(searchParams.get('query'))
+    const [CardData, setCardData] = useState([])
     const [DataIsMissing, setDataIsMissing] = useState(true)
-    const [SearchResultStatus, setSearchResultStatus] = useState(false)
     const [Loading, setLoading] = useState(true)
 
     useEffect(() => {
-      FetchPersons()
       setQuery(searchParams.get('query'))
+      FetchPersons()
     }, [])
 
     const FetchPersons = () => {
-      fetch(process.env.REACT_APP_API_URL + "/person", {
+      fetch(`${process.env.REACT_APP_API_URL}/person/`, {
         method: 'GET',
         redirect: 'follow'
       })
       .then(res => res.json())
       .then(result => {
         if(ValidateCardData(result)) {
-          setCardData(FilterData(result))
+          FilterCardData(result)
           setDataIsMissing(false)
         }
         setLoading(false)
       })
       .catch(error => {
-        alert(error)
         setLoading(false)
       }) 
+    }
+
+    const RenderCards = () => { 
+      return (
+        CardData.map(e => (
+          <div className="col-md">
+            <Card data={e} />
+        </div>
+        ))
+      )
     }
 
     const ValidateCardData = (res) => {
@@ -44,43 +53,32 @@ const SearchResult = () => {
       return true
     }
 
-    const FilterData = result => {
-      let FilteredData = []
+    const FilterCardData = (res) => {
+      let cardData = []
 
-      result.map(e => {
-        if(e.name.includes(Query))
-          FilteredData.push(e)
+      res.map(e => {
+        if(e.name.toLowerCase().includes(Query)) {
+          cardData.push(e)
+        }
       })
 
-      return FilteredData
-    }
-
-    const RenderCards = () => { 
-      return (
-        CardData.map(e => (
-          <div className="col-md">
-            <Card   
-              key={e._id}
-              id={e._id}
-              img={e.img}
-              name={e.name}
-              link={e.link}
-              summary={e.summary}
-            />
-        </div>
-        ))
-      )
+      setCardData(cardData)
     }
 
     const VerifyIfDataIsMissing = () => {
       if (DataIsMissing) 
-        return <MissingData title="Nenhuma resultado encontrado"/>
-      
+        return <MissingData title="Nenhuma pessoa encontrada"/>
+
+      if (CardData.length === 0) 
+        return <MissingData title="Nenhuma pessoa encontrada"/>
+
+      else {
       return (
         <div className="row row-cols-1 row-cols-md-2 g-4">
           {RenderCards()}
         </div>
       )
+    }
   } 
     
   if(Loading) {
@@ -88,10 +86,6 @@ const SearchResult = () => {
       <div className="App">
       <Header />
       <div className="container">
-      <h1 className="title mb-3">Resultados:</h1>
-        <p>
-          {searchParams.get('query')}
-        </p>
         <Loader />
       </div>
     </div>
@@ -102,15 +96,13 @@ const SearchResult = () => {
       <div className="App">
         <Header />
         <div className="container">
-        <h1 className="title mb-3">Resultados:</h1>
-        <p>
-          {searchParams.get('query')}
-        </p>
+          <h1 className="title mb-3">Cancelados:</h1>
+          {Query}
           {VerifyIfDataIsMissing()} 
         </div>
       </div>
     )
   }
 }
-
+  
 export default SearchResult
